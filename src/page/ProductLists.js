5ddbdbf79523e9a8f3f4cc4a4c537c41e6cd26d0
unknown <seon.xero@gmail.com>
2025-02-04 19:20:57 +0900

    db수정 및 데이터배열화

--- src/page/ProductLists.js
+++ src/page/ProductLists.js
@@ -2,29 +2,35 @@ import React from 'react';
 import {useState, useEffect} from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
+const products = [
+  {
+    id: "pan",
+    title: "프라이팬",
+    subtitle: "대한민국 프라이팬의 자존심<br/> 소재와 코팅 재질, 형태별로 펼쳐지는 다양한 HC* 프라이팬 라인업",
+    img: "../img/a0.jpg",
+    lists: [
+      { id: 0, title: "플렉스팬 시리즈", subtitle: "굽고, 볶고, 끓여야 하는 어떤 요리도 플렉스팬 시리즈 하나로 간편하게", img: "/img/a1.jpg" },
+      { id: 1, title: "컴플리트 시리즈", subtitle: "요리의 시작부터 완성까지 어떤 요리에도 늘 찾게 되는 팬", img: "/img/a2.jpg" },
+      { id: 2, title: "그래핀 시리즈", subtitle: "21세기 검은 황금 '그래핀' 신소재 그래핀, 예열시간의 놀라운 감소", img: "/img/a3.jpg" },
+      { id: 3, title: "포레스트 시리즈", subtitle: "숲에서 온 디자인 주방에서 피톤치드 가득한 숲속 분위기", img: "/img/a4.jpg" }
+    ]
+  },
+  {
+    id: "pot",
+    title: "냄비",
+    subtitle: "다양한 크기와 깊이로 요리를 더욱 편리하게",
+    img: "../img/b0.jpg",
+    lists: [
+      { id: 0, title: "스테인리스 냄비", subtitle: "내구성이 뛰어난 스테인리스 냄비", img: "/img/b1.jpg" },
+      { id: 1, title: "양수 냄비", subtitle: "국물 요리에 최적화된 디자인", img: "/img/b2.jpg" },
+      { id: 2, title: "전골 냄비", subtitle: "전골 요리에 적합한 얇고 넓은 냄비", img: "/img/b3.jpg" }
+    ]
+  }
+];
+
 const ProductLists = () => {
 
-  const [products, setProducts] = useState([]); 
-  const [selectProduct, setSelectProduct] = useState(null);
-
-  const getProductData = async () => {
-    try {
-      const response = await fetch("https://my-json-server.typicode.com/seonyoungg/happyCall/products");
-      const data = await response.json();
-      if (Array.isArray(data)) {
-        setProducts(data);
-        setSelectProduct(data.length > 0 ? data[0] : null);
-      } else {
-        throw new Error("Fetched data is not an array");
-      }
-    } catch (error) {
-      console.error("Error fetching products:", error);
-    }
-  };
-
-  useEffect(() => {
-    getProductData();
-  }, []);
+  const [selectProduct, setSelectProduct] = useState(products[0]); // 기본값: 프라이팬
 
   return (
     <div className="subpage">
